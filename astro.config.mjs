97614df052189063154638374818d6004b5279fb
Ryan <ryangst.hire@gmail.com>
2025-11-30 13:51:42 -0300

    chore: add custom robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,63 +7,63 @@ import starlightLinksValidator from "starlight-links-validator";
 import cloudflare from "@astrojs/cloudflare";
 import { config } from "./src/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: config.site,
-  integrations: [
-    starlight({
-      favicon: "/favicon.ico",
-      title: config.title,
-      description: config.description,
-      plugins: [
-        starlightLinksValidator(),
-        starlightGiscus({
-          repo: "ryangst/guides",
-          repoId: "R_kgDOQTxF8A",
-          category: "Q&A",
-          categoryId: "DIC_kwDOQTxF8M4CxsGx",
-          lazy: true,
-        }),
-      ],
-      logo: {
-        src: "./src/assets/axe_logo.png",
-        alt: "ryangst Logo",
-        replacesTitle: true,
+  integrations: [starlight({
+    favicon: "/favicon.ico",
+    title: config.title,
+    description: config.description,
+    plugins: [
+      starlightLinksValidator(),
+      starlightGiscus({
+        repo: "ryangst/guides",
+        repoId: "R_kgDOQTxF8A",
+        category: "Q&A",
+        categoryId: "DIC_kwDOQTxF8M4CxsGx",
+        lazy: true,
+      }),
+    ],
+    logo: {
+      src: "./src/assets/axe_logo.png",
+      alt: "ryangst Logo",
+      replacesTitle: true,
+    },
+    routeMiddleware: "./src/routeData.ts",
+    customCss: ["./src/styles/custom.css"],
+    locales: {
+      root: {
+        label: "English",
+        lang: "en",
       },
-      routeMiddleware: "./src/routeData.ts",
-      customCss: ["./src/styles/custom.css"],
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        "pt-br": {
-          label: "Português (Brasil)",
-          lang: "pt-BR",
+      "pt-br": {
+        label: "Português (Brasil)",
+        lang: "pt-BR",
+      },
+    },
+    defaultLocale: "root",
+    social: config.social,
+    editLink: {
+      baseUrl: "https://github.com/RyanGst/guides/edit/main",
+    },
+    sidebar: [
+      {
+        label: "Guides",
+        autogenerate: { directory: "guides" },
+        translations: {
+          "pt-br": "Guias",
         },
       },
-      defaultLocale: "root",
-      social: config.social,
-      editLink: {
-        baseUrl: "https://github.com/RyanGst/guides/edit/main",
+      {
+        label: "webdev",
+        autogenerate: { directory: "webdev" },
+        translations: {
+          "pt-br": "Desenvolvimento Web",
+        },
       },
-      sidebar: [
-        {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-          translations: {
-            "pt-br": "Guias",
-          },
-        },
-        {
-          label: "webdev",
-          autogenerate: { directory: "webdev" },
-          translations: {
-            "pt-br": "Desenvolvimento Web",
-          },
-        },
-      ],
-    }),
-  ],
+    ],
+  }), sitemap()],
   adapter: cloudflare(),
 });
